d694f63cde24f7223166ee16ba4e63033aa89852
Singh <sabhisheksignh343104@gmail.com>
2023-07-07 16:32:08 +0530

    some comments added

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import colorama
 from colorama import Fore
 colorama.init(autoreset=True)
-import requests
+import requests  # requests python library for scrapping data from openweathermap.org
 
+# Include your own opneweathermap API Key here, visit https://api.openweathermap.org/
 API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
 
+# Defining a function a that takes city name and request weather info of the city, scrap the data from returned output
 def weath(city_name):
 	try:
-		data = requests.get("https://api.openweathermap.org/data/2.5/weather?q="+city_name+f"&appid={API_KEY}").json()
+		# Requesting weather info from openweathermap.org website
+		data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}").json()
 
 		climate = "Weather climate"
 		temp = "Temperature"
@@ -18,8 +21,8 @@ def weath(city_name):
 		hum = "Humidity"
 		latlon = "Lat and lon"
 
-		print(f"{Fore.BLUE}\nWeather in",f"{Fore.GREEN}"+city_name.title()+"("+f"{Fore.GREEN}"+str(data["sys"]["country"])+")")
-		print()
+		# Scrapping recieved data
+		print(f"{Fore.BLUE}\nWeather in",f"{Fore.GREEN}"+city_name.title()+"("+f"{Fore.GREEN}"+str(data["sys"]["country"])+")\n")
 		print(f"{Fore.CYAN}{climate:>25} : ",data["weather"][0]["main"])
 		print(f"{Fore.CYAN}{temp:>25} : ",str(int(data["main"]["temp"]-273.15))+"°C")
 		print(f"{Fore.CYAN}{desc:>25} : ",data["weather"][0]["description"])
@@ -30,10 +33,9 @@ def weath(city_name):
 		print(f"{Fore.CYAN}{latlon:>25} : ",data["coord"]["lat"],"and",data["coord"]["lon"])
 		print()
 	except:
-		print(f"{Fore.RED}Opps, something went wrong!\n")
-		print()
+		print(f"{Fore.RED}\nOpps, something went wrong!\n")
 if __name__ == "__main__":
 	print(f"{Fore.CYAN}\n\t\t********** How is Weather in Your City?",f"{Fore.CYAN}**********\n")
-	print()
+	# Input city name
 	usr = input("Enter your city: ")
 	weath(usr)
